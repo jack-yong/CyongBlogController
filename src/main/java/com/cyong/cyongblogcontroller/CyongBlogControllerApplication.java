ae0082727fe117a289358298191de0032810a3bf
jack-yong <1807413446@qq.com>
2021-10-17 20:31:37 +0800

    第一次提交

---
+++ src/main/java/com/cyong/cyongblogcontroller/CyongBlogControllerApplication.java
@@ -0,0 +1,13 @@
+package com.cyong.cyongblogcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyongBlogControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyongBlogControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyong/cyongblogcontroller/CyongBlogControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyong.cyongblogcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyongBlogControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
